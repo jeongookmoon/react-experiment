f75e9ace2cb256dd6be1a0dd4e0d84d061493e83
Jeong Ook Moon <moonjeongook@gmail.com>
2021-03-23 15:56:59 -0700

    added column sort

--- src/pages/userTable/components/table.component.tsx
+++ src/pages/userTable/components/table.component.tsx
@@ -1,16 +1,11 @@
 import React, { ReactElement, useMemo } from 'react';
-import { useTable } from 'react-table';
-import { GROUPED_COLUMNS, IColumn } from '../constants/columns';
+import { useTable, useSortBy } from 'react-table';
+import { GROUPED_COLUMNS } from '../constants/columns';
 import MOCK_DATA from '../../../../mock_data/data__id_name_email_gender_ip.json';
 
 function TableComponent(): ReactElement {
   const columns = useMemo(() => GROUPED_COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
-
-  const tableInstance = useTable<IColumn>({
-    columns,
-    data,
-  });
 
   const {
     getTableProps,
@@ -18,7 +13,13 @@ function TableComponent(): ReactElement {
     headerGroups,
     rows,
     prepareRow,
-  } = tableInstance;
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
 
   return (
     <table {...getTableProps()}>
@@ -30,8 +31,22 @@ function TableComponent(): ReactElement {
             key={'tableRow' + trIndex}
           >
             {aHeaderGroup.headers.map((aColumn, thIndex) => (
-              <th {...aColumn.getHeaderProps()} key={'tableHeader' + thIndex}>
+              <th
+                {...aColumn.getHeaderProps(aColumn.getSortByToggleProps())}
+                style={{
+                  background: 'white',
+                  maxWidth: "50px"
+                }}
+                key={'tableHeader' + thIndex}
+              >
                 {aColumn.render('Header')}
+                <span>
+                  {aColumn.isSorted
+                    ? aColumn.isSortedDesc
+                      ? ' ▼'
+                      : ' ▲'
+                    : '  '}
+                </span>
               </th>
             ))}
           </tr>
