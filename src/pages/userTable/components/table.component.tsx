06b72680ddefd5fb5780427351eaec58ad458aba
Jeong Ook Moon <moonjeongook@gmail.com>
2021-03-27 17:29:43 -0700

    implmented inline edit & added states display

--- src/pages/userTable/components/table.component.tsx
+++ src/pages/userTable/components/table.component.tsx
@@ -1,24 +1,118 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement, useEffect, useMemo, useState } from 'react';
 import {
   useTable,
   useSortBy,
   useGlobalFilter,
   useFilters,
   usePagination,
+  Hooks,
 } from 'react-table';
 import { GROUPED_COLUMNS } from '../constants/columns';
 import MOCK_DATA from '../../../../mock_data/data__id_name_email_gender_ip.json';
 import { SearchComponent } from './search.component';
 import { ColumnFilter } from './columnFilter.component';
 
+type typeRowValues = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+};
+const NON_SELECTED = 'none-selected';
+const PRIMARY_KEY = 'id';
+
 function TableComponent(): ReactElement {
   const columns = useMemo(() => GROUPED_COLUMNS, []);
-  const data = useMemo(() => MOCK_DATA, []);
+  const mockData = useMemo(() => MOCK_DATA, []);
+
+  const [selectedRowId, selectedRowIdSet] = useState(NON_SELECTED);
+  const [selectedRowValues, selectedRowValuesSet] = useState<
+    typeRowValues | undefined
+  >(undefined);
+  const [beginEdit, beginEditSet] = useState(false);
+  const [data, dataSet] = useState(mockData);
+  const [editedRowValues, editedRowValuesSet] = useState<
+    typeRowValues | undefined
+  >(undefined);
+
+  const updateTableData = () => {
+    if (editedRowValues) {
+      dataSet(
+        data.map((row, index) => {
+          if (index === Number(editedRowValues.id) - 1) {
+            return editedRowValues;
+          }
+          return row;
+        })
+      );
+    }
+  };
+
+  const updateEditedRowData = ({
+    rowValues,
+    columnId,
+    cellValue,
+  }: // TODO: put explicit type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Hooks<any>): void => {
+    const newData = editedRowValues
+      ? { ...editedRowValues, [columnId]: cellValue }
+      : { ...rowValues, [columnId]: cellValue };
+
+    editedRowValuesSet(newData);
+  };
+
+  const EditableCell = ({
+    value: initialCellValue,
+    row: { index, values },
+    column: { id },
+    updateEditedRowData,
+  }: {
+    value: string;
+    row: { index: number; values: typeRowValues };
+    column: { id: string };
+    updateEditedRowData: ({
+      rowId,
+      rowValues,
+      columnId,
+      cellValue,
+    }: {
+      rowId: number;
+      rowValues: typeRowValues;
+      columnId: string;
+      cellValue: string;
+    }) => void;
+  }) => {
+    const [cellValue, cellValueSet] = useState(initialCellValue);
+    const parame = {
+      rowId: index,
+      rowValues: values,
+      columnId: id,
+      cellValue,
+    };
+
+    useEffect(() => {
+      cellValueSet(initialCellValue);
+    }, [initialCellValue]);
+
+    return (
+      <input
+        value={cellValue}
+        onChange={event => {
+          cellValueSet(event.target.value);
+        }}
+        onBlur={() => updateEditedRowData(parame)} // set new row data
+      />
+    );
+  };
 
   const defaultColumn = useMemo(
-    // pre-requisite to redner('Filter')
+    // defines component for render; ex) aColumn.render('Filter') => ColumnFilter component rendered
     () => ({
       Filter: ColumnFilter,
+      Cell: EditableCell,
     }),
     []
   );
@@ -28,7 +122,7 @@ function TableComponent(): ReactElement {
     getTableBodyProps,
     headerGroups,
     prepareRow,
-    state, // for Global Filter
+    state: { globalFilter, pageIndex, pageSize },
     setGlobalFilter, // for Global Filter
     page,
     nextPage,
@@ -43,7 +137,8 @@ function TableComponent(): ReactElement {
     {
       columns,
       data,
-      defaultColumn, // pre-requisite to redner('Filter')
+      defaultColumn, // custom component
+      updateEditedRowData, // custom function
     },
     useFilters,
     useGlobalFilter,
@@ -51,10 +146,42 @@ function TableComponent(): ReactElement {
     usePagination
   );
 
-  const { globalFilter, pageIndex, pageSize } = state;
-
   return (
     <>
+      <p>
+        #GlobalSearch, #InlineEdit, #ColumnSorting, #ColumnSearch, #Pagination,
+        #PageNavigator{' '}
+      </p>
+      <button
+        type="button"
+        disabled={beginEdit}
+        onClick={() => {
+          if (selectedRowValues !== undefined) beginEditSet(true);
+        }}
+      >
+        Edit
+      </button>
+      <button
+        type="button"
+        disabled={!beginEdit}
+        onClick={() => {
+          beginEditSet(false);
+          updateTableData();
+          editedRowValuesSet(undefined);
+        }}
+      >
+        Update
+      </button>
+      <button
+        type="button"
+        disabled={!beginEdit}
+        onClick={() => {
+          beginEditSet(false);
+          editedRowValuesSet(undefined);
+        }}
+      >
+        Cancel
+      </button>
       <SearchComponent filter={globalFilter} filterSet={setGlobalFilter} />
       <table {...getTableProps()}>
         <thead>
@@ -69,13 +196,10 @@ function TableComponent(): ReactElement {
                   {...aColumn.getHeaderProps()}
                   style={{
                     background: 'white',
-                    maxWidth: '50px',
                   }}
                   key={'tableHeader' + thIndex}
                 >
-                  <div>
-                    {aColumn.canFilter ? aColumn.render('Filter') : null}
-                  </div>
+                  {/* ColumnFilter */}
                   <div>
                     {aColumn.render('Header')}
 
@@ -89,6 +213,11 @@ function TableComponent(): ReactElement {
                         ? ' 🔃'
                         : ''}
                     </span>
+                    <span>
+                      {aColumn.id === 'gender' && aColumn.canFilter
+                        ? aColumn.render('Filter')
+                        : null}
+                    </span>
                   </div>
                 </th>
               ))}
@@ -100,11 +229,56 @@ function TableComponent(): ReactElement {
           {page.map((aRow, tbRowIndex) => {
             prepareRow(aRow);
             return (
-              <tr {...aRow.getRowProps()} key={'tableBodyRow' + tbRowIndex}>
+              <tr
+                {...aRow.getRowProps()}
+                key={'tableBodyRow' + tbRowIndex}
+                style={
+                  selectedRowId === aRow.id
+                    ? {
+                        transition: '.2s',
+                        backgroundColor: 'skyblue',
+                        cursor: 'pointer',
+                      }
+                    : { backgroundColor: 'white', cursor: 'pointer' }
+                }
+              >
                 {aRow.cells.map((aCell, cellIndex) => {
                   return (
-                    <td {...aCell.getCellProps()} key={'cell' + cellIndex}>
-                      {aCell.render('Cell')}
+                    <td
+                      {...aCell.getCellProps()}
+                      key={'cell' + cellIndex}
+                      onClick={() => {
+                        let rowId = aCell.row.id;
+                        let rowValues: typeRowValues | undefined = aCell.row
+                          .values as typeRowValues;
+
+                        if (beginEdit) {
+                          if (selectedRowId !== aCell.row.id) {
+                            beginEditSet(false);
+                          }
+                        } else {
+                          rowId =
+                            selectedRowId === aCell.row.id
+                              ? NON_SELECTED
+                              : aCell.row.id;
+
+                          rowValues =
+                            selectedRowId === aCell.row.id
+                              ? undefined
+                              : (aCell.row.values as typeRowValues);
+                        }
+
+                        selectedRowIdSet(rowId);
+                        selectedRowValuesSet(rowValues as typeRowValues);
+                      }}
+                    >
+                      {beginEdit &&
+                      selectedRowId === aCell.row.id &&
+                      aCell.column.id !== PRIMARY_KEY ? (
+                        aCell.render('Cell')
+                      ) : (
+                        <div>{aCell.value}</div>
+                      )}
                     </td>
                   );
                 })}
@@ -113,30 +287,47 @@ function TableComponent(): ReactElement {
           })}
         </tbody>
       </table>
-      <div>
-        <br />
-        <span>
-          Page{' '}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}{' '}
-          </strong>
-        </span>
-        <span>
-          | Go to page:{' '}
-          <input
-            type="number"
-            value={pageIndex + 1}
-            onChange={event => {
-              const pageNumber = event.target.value
-                ? Number(event.target.value) - 1
-                : 0;
-              gotoPage(pageNumber);
-            }}
-            style={{ width: '50px' }}
-            min={1}
-            max={pageOptions.length}
-          />{' '}
-        </span>
+      <br />
+      {/* Navigator */}
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>
+      </div>
+      <br />
+      <div style={{ textAlign: 'center' }}>
+        Go to page:{' '}
+        <input
+          type="number"
+          value={pageIndex + 1}
+          onChange={event => {
+            const pageNumber = event.target.value
+              ? Number(event.target.value) - 1
+              : 0;
+            gotoPage(pageNumber);
+          }}
+          min={1}
+          max={pageOptions.length}
+        />{' '}
+      </div>
+      <br />
+      <div style={{ textAlign: 'center' }}>
+        Page{' '}
+        <strong>
+          {pageIndex + 1} of {pageOptions.length}{' '}
+        </strong>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <br /># of Rows per Page{' '}
         <select
           value={pageSize}
           onChange={event => setPageSize(Number(event.target.value))}
@@ -147,19 +338,25 @@ function TableComponent(): ReactElement {
             </option>
           ))}
         </select>{' '}
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {'<<'}
-        </button>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          Previous
-        </button>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          Next
-        </button>
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {'>>'}
-        </button>
       </div>
+      <br />
+      <br />
+      <p>States:</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowId,
+              selectedRowValues,
+              beginEdit,
+              globalFilter,
+              editedRowValues,
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
     </>
   );
 }
